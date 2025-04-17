0549967b5ca3150c6f880d0b069a9f4947ba6652
doubleoh13 <doubleoh13@doubleoh13.dev>
2025-04-17 14:19:33 -0400

    refactor: extract artisan command logic to reusable function
    - Moved inline artisan command resolution logic to `_get_artisan_cmd()`
    - Added support for ARTISAN_COMMAND and ARTISAN_SERVICE from `.env` or env vars
    - Improved handling of docker-compose service resolution
    - Rewrote completions to use `list --format=json` with `jq` for accuracy
    - Minor cleanup: replaced legacy backticks, used `$(...)` consistently, and safer argument handling

--- artisan.plugin.zsh
+++ artisan.plugin.zsh
@@ -7,36 +7,18 @@
 # adds shell completions that work anywhere artisan can be located.
 
 function artisan() {
-    local artisan_path=`_artisan_find`
+    local artisan_path=$(_artisan_find)
 
     if [ "$artisan_path" = "" ]; then
         >&2 echo "zsh-artisan: artisan not found. Are you in a Laravel directory?"
         return 1
     fi
 
-    local laravel_path=`dirname $artisan_path`
-    local docker_compose_config_path=`find $laravel_path -maxdepth 1 \( -name "docker-compose.yml" -o -name "docker-compose.yaml" \) | head -n1`
-    local artisan_cmd
-
-    if [ "$docker_compose_config_path" = '' ]; then
-        artisan_cmd="php $artisan_path"
-    else
-        if [ "`grep "laravel/sail" $docker_compose_config_path | head -n1`" != '' ]; then
-            artisan_cmd="$laravel_path/vendor/bin/sail artisan"
-        else
-            local docker_compose_cmd=`_docker_compose_cmd`
-            local docker_compose_service_name=`$=docker_compose_cmd ps --services 2>/dev/null | grep 'app\|php\|api\|workspace\|laravel\.test\|webhost' | head -n1`
-            if [ -t 1 ]; then
-                artisan_cmd="$docker_compose_cmd exec $docker_compose_service_name php artisan"
-            else
-                # The command is not being run in a TTY (e.g. it's being called by the completion handler below)
-                artisan_cmd="$docker_compose_cmd exec -T $docker_compose_service_name php artisan"
-            fi
-        fi
-    fi
+    local artisan_cmd=$(_get_artisan_cmd true)
+    [[ -z "$artisan_cmd" ]] && return 1
 
     local artisan_subcmd=$1
-    shift
+    (( $# > 0 )) && shift
 
     local artisan_args=()
     local arg
@@ -45,9 +27,9 @@ function artisan() {
         artisan_args+=($(printf "%s" "$arg" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/'/"))
     done
 
-    local artisan_start_time=`date +%s`
+    local artisan_start_time=$(date +%s)
 
-    eval $artisan_cmd $artisan_subcmd $artisan_args
+    eval "$artisan_cmd $artisan_subcmd $artisan_args"
 
     local artisan_exit_status=$? # Store the exit status so we can return it later
 
@@ -58,7 +40,7 @@ function artisan() {
             "$laravel_path/tests" \
             "$laravel_path/database" \
             -type f \
-            -newermt "-$((`date +%s` - $artisan_start_time + 1)) seconds" \
+            -newermt "-$(($(date +%s) - $artisan_start_time + 1)) seconds" \
             -exec $ARTISAN_OPEN_ON_MAKE_EDITOR {} \; 2>/dev/null
     fi
 
@@ -66,6 +48,60 @@ function artisan() {
 }
 
 compdef _artisan_add_completion artisan
+
+function _get_artisan_cmd() {
+    local use_tty=$1
+
+    local artisan_path=$(_artisan_find)
+    if [[ -z "$artisan_path" ]]; then
+        >&2 echo "zsh-artisan: artisan not found. Are you in a Laravel directory?"
+        return 1
+    fi
+
+    local laravel_path=$(dirname $artisan_path)
+    local env_path="$laravel_path/.env"
+
+    local file_command=
+    local file_service=
+
+    if [[ -f "$env_path" ]]; then
+        local file_command=$(_get_env_value_from_file $env_path "ARTISAN_COMMAND")
+        local file_service=$(_get_env_value_from_file $env_path "ARTISAN_SERVICE")
+    fi
+    
+    local effective_command="${file_command:-${ARTISAN_COMMAND:-}}"
+    local effective_service="${file_service:-${ARTISAN_SERVICE:-}}"
+    
+    if [[ -n "$effective_command" ]]; then
+        # If the command is set in the .env file, use it
+        echo "$effective_command"
+        return 0
+    fi
+    
+    local docker_compose_config_path=$(find $laravel_path -maxdepth 1 \( -name "docker-compose.yml" -o -name "docker-compose.yaml" \) | head -n1)
+
+    if [[ -z "$docker_compose_config_path" ]]; then
+        echo "php $artisan_path"
+    else
+        if [[ "$(grep "laravel/sail" $docker_compose_config_path | head -n1)" != '' ]]; then
+            echo "$laravel_path/vendor/bin/sail artisan"
+        else
+            local docker_compose_cmd=$(_docker_compose_cmd)
+            local service="${effective_service:-$($=docker_compose_cmd ps --services 2>/dev/null | grep -E 'app|php|api|workspace|laravel\.test|webhost' | head -n1)}"
+
+            if [[ -z "$service" ]]; then
+                >&2 echo "zsh-artisan: unable to determine docker service name"
+                return 1
+            fi
+
+            if [[ "$use_tty" == true ]]; then
+                echo "$docker_compose_cmd exec $service php artisan"
+            else
+                echo "$docker_compose_cmd exec -T $service php artisan"
+            fi
+        fi
+    fi
+}
 
 function _artisan_find() {
     # Look for artisan up the file tree until the root directory
@@ -83,13 +119,17 @@ function _artisan_find() {
 }
 
 function _artisan_add_completion() {
-    if [ "`_artisan_find`" != "" ]; then
-        compadd `_artisan_get_command_list`
+    if [ -n "$(_artisan_find)" ]; then
+        compadd $(_artisan_get_command_list)
     fi
 }
 
 function _artisan_get_command_list() {
-    artisan --raw --no-ansi list | sed "s/[[:space:]].*//g"
+    local artisan_cmd=$(_get_artisan_cmd true)
+    [[ -z "$artisan_cmd" ]] && return 1
+
+    eval "$artisan_cmd list --format=json" 2>/dev/null | jq -r '.namespaces[].commands | values[]'
+    #artisan --raw --no-ansi list | sed "s/[[:space:]].*//g"
 }
 
 function _docker_compose_cmd() {
@@ -99,4 +139,12 @@ function _docker_compose_cmd() {
     else
         echo "docker-compose"
     fi
+}
+
+function _get_env_value_from_file() {
+    local file=$1
+    local key=$2
+
+    grep -E "^$key=" "$file" 2>/dev/null | tail -n1 | cut -d '=' -f2- | sed -e 's/^"//' -e 's/"$//'
+
 }
